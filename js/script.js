f86b9b649ac46aa683840789e1f65e9fb99aebbc
“jdelavega94” <jcdelavega94@gmail.com>
2023-03-09 17:51:33 +0100

    milestone 1

--- js/script.js
+++ js/script.js
@@ -3,31 +3,54 @@ const pricePerKm = 0.21;
 const juniorDiscount = 20;
 const seniorDiscount = 40;
 
-// @raccolta dati
+// @data collection
 const js_submit = document.getElementById("submit");
 js_submit.addEventListener("click", function() {
 
     // dati acquisiti tramite input
     const userNameInput = document.getElementById("username");
     const js_username = userNameInput.value;
-    const kilometerInput = document.getElementById("kilometer");
-    const js_kilometer = kilometerInput.value;
+    const kilometerInput =  document.getElementById("kilometer");
+    const js_kilometer = parseFloat(kilometerInput.value);
     const ageInput = document.getElementById("age");
     const js_age = ageInput.value;
-
     console.log(js_username, js_kilometer, js_age);
     
-    // @logica
+    // @logic
+    // #ticket price
     const ticket = kilometerInput.value * pricePerKm;
     console.log(ticket);
-    let price = '';
-    if (ageInput == "junior") {
+    let price = ticket;
+    if (ageInput.value === "junior") {
         price = ticket - ticket * juniorDiscount / 100;
+    } else if (ageInput.value === "senior"){
+        price = ticket - ticket * seniorDiscount /100;
     }
-    console.log(ticket);
+    console.log(price);
+    // #ticket type
+    let promotion = "Biglietto standard";
+    if (ageInput.value === "junior") {
+        promotion = `Sconto 20%`;
+    } else if (ageInput.value === "senior") {
+        promotion = `sconto 40%`;
+    }
+    console.log(promotion);
+    // #coach number
+    let coach = Math.floor((Math.random() * 10) + 1);
+    console.log(coach);
+    // #code number
+    let code = Math.floor((Math.random() * 88888) + 11111);
+    console.log(code);
+
+    // @output
+    userOutput.innerHTML = `${js_username}`;
+    promotionOutput.innerHTML = `${promotion}`;
+    coachOutput.innerHTML = `${coach}`;
+    codeOutput.innerHTML = `${code}`;
+    finalPrice.innerHTML = `\u20AC${price.toFixed(2)}`
 });
 
-// @reset dati
+// @data reset
 const js_reset = document.getElementById("reset");
 js_reset.addEventListener("click", function() {
     const userNameInput = document.getElementById("username");
@@ -37,4 +60,4 @@ js_reset.addEventListener("click", function() {
     userNameInput.value = ""
     kilometerInput.value = ""
     ageInput.value = ""
-})
+});
